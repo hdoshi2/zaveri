e7d296390293d575d9d30c5642a6f9072a367945
Hari.Doshi <hdoshi2@gmail.com>
2019-09-25 17:47:29 -0400

    style: minor changes to add order form

---
+++ client/store/customer.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_CUSTOMER = 'GET_CUSTOMER'
+
+
+/**
+ * ACTION CREATORS
+ */
+const getCustomer = customer => ({type: GET_CUSTOMER, customer})
+
+
+/**
+ * THUNK CREATORS
+ */
+
+export function fetchCustomer() {
+  return async dispatch => {
+    try{
+      const data = await axios.get('/api/customer')
+      dispatch(getCustomer(data))
+    } catch(err) {
+      console.log('error in fetchCustomer thunk')
+    }
+  }
+}
+
+/**
+ * INITIAL STATE
+ */
+const defaultState = {isloading: true, customerList: []}
+
+/**
+ * REDUCER
+ */
+
+ export const customerReducer = (state = defaultState, action){
+   
+ }
