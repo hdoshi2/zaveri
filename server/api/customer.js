effcd8033348a29a1e2ad9d4c4918013f3688bb5
Hari.Doshi <hdoshi2@gmail.com>
2019-09-25 20:43:46 -0400

    feat: created new customer post route and connected to db

--- server/api/customer.js
+++ server/api/customer.js
@@ -5,11 +5,18 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const customer = await Customer.findAll({
-      order: [
-        ['lastName', 'DESC'],
-    ]
+      order: [['lastName', 'DESC']]
     })
     res.json(customer)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newCustomer = await Customer.create(req.body)
+    res.status(201).send(newCustomer)
   } catch (err) {
     next(err)
   }
